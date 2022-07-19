e088baf85500c33f8754a9c8d7ca21c3fff6f2a0
FernandoStark18 <fernandostark18@gmail.com>
2022-07-18 18:33:23 -0600

    Variables con let y cosnt

---
+++ js/03_let.js
@@ -0,0 +1,20 @@
+// Variables con let
+
+let producto = 'Audífonos Gamer'; // Iniciar variable y asignarle valor
+
+let disponible; // Iniciar variable sin valor
+
+producto = true;
+
+disponible = true;
+
+let producto = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+// Estilos para las variables
+
+let nomnre_producto = 'Monitor HD'; // underscore
+let nombreProducto = 'Monitor HD';  // Camelcase
+let NombreProducto = 'Monitor HD'; // Pascal Case
+let nombreproducto = 'Monitor HD';
---
+++ js/04_const.js
@@ -0,0 +1,20 @@
+// Variables con const
+
+const producto = 'Audífonos Gamer'; // Iniciar variable y asignarle valor
+
+const disponible; // Iniciar variable sin valor
+
+producto = true;
+
+disponible = true;
+
+const producto = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+// Estilos para las variables
+
+const nomnre_producto = 'Monitor HD'; // underscore
+const nombreProducto = 'Monitor HD';  // Camelcase
+const NombreProducto = 'Monitor HD'; // Pascal Case
+const nombreproducto = 'Monitor HD';
