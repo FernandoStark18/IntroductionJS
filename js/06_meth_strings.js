7d9ecbf52d5e818bfcb67098367cb55266abcebd
FernandoStark18 <fernandostark18@gmail.com>
2022-07-18 19:25:02 -0600

    Métodos de variables tipo String

---
+++ js/06_meth_strings.js
@@ -0,0 +1,12 @@
+const tweet = "Aprendiendo JavaScript con el curso de Desarrollo Web Completo";
+const producto2 = String('Monitor HD');
+
+// length
+console.log(tweet.length);
+console.log(producto2);
+
+// indexOf (retorna posición)
+console.log(tweet.indexOf('JavaScript'));
+
+// includes (retorna true o false)
+console.log(tweet.includes('curso'));
