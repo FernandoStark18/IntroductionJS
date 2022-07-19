3936ade4f4ddc2b6b0c99f9ebd092752fafcd6c8
FernandoStark18 <fernandostark18@gmail.com>
2022-07-18 18:18:00 -0600

    Variables en Javascript

---
+++ js/variables.js
@@ -0,0 +1,22 @@
+// Variables
+
+var producto = 'Audífonos Gamer'; // Iniciar variable y asignarle valor
+
+var disponible; // Iniciar variable sin valor
+
+producto = true;
+
+disponible = true;
+
+var producto = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+// Estilos para las variables
+
+var nomnre_producto = 'Monitor HD'; // underscore
+var nombreProducto = 'Monitor HD';  // Camelcase
+var NombreProducto = 'Monitor HD'; // Pascal Case
+var nombreproducto = 'Monitor HD'; 
+
+console.log(producto);
