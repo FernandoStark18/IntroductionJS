550575c4846536907e2bd9ce3b7b44a23538d42c
FernandoStark18 <fernandostark18@gmail.com>
2022-07-19 20:04:05 -0600

    Objeto Math

---
+++ js/08_math.js
@@ -0,0 +1,15 @@
+// Objeto Math
+
+let resultado;
+
+resultado = Math.PI;
+resultado = Math.round(2.6);
+resultado = Math.ceil(2.2); // Ceil siempre redondea hacia arriba
+resultado = Math.floor(2.9); // Floor siempre redondea hacia abajo
+resultado = Math.sqrt(144);
+resultado = Math.min(5, 8 ,3);
+resultado = Math.max(5, 8 ,3);
+resultado = Math.random();
+resultado = Math.floor(Math.random() * 100);
+
+console.log(resultado);
