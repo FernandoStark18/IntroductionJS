37ce59d3ce9fae99134c039216a041b8a52483b9
FernandoStark18 <fernandostark18@gmail.com>
2022-07-20 19:18:04 -0600

    Concatenación y Template Strings

---
+++ js/10_concat&template_strings.js
@@ -0,0 +1,8 @@
+const nombre = "Fernando";
+const email = "micorreo@correo.com";
+
+// Concatenación
+console.log("Nombre cliente: " + nombre + " " + email);
+
+// Template Strings - String literals
+console.log(`Nombre cliente: ${nombre} Email: ${email}`);
