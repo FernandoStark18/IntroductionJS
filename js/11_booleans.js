748d1f4224c711cf05344cd38efd2aa3134b77f3
FernandoStark18 <fernandostark18@gmail.com>
2022-07-20 19:24:00 -0600

    Booleans

---
+++ js/11_booleans.js
@@ -0,0 +1,11 @@
+/// Booleans
+const boolean1 = true;
+const boolean2 = false;
+const boolean3 = "true";
+const boolean4 = new Boolean(true);
+
+console.log(`Var: ${boolean1} Type: ${typeof boolean1}`);
+console.log(`Var: ${boolean2} Type: ${typeof boolean2}`);
+console.log(`Var: ${boolean3} Type: ${typeof boolean3}`);
+
+console.log(`Var: ${boolean4} Type: ${typeof boolean4}`);
