a14833853c4c6d73f403715139c5bf4889e7a510
FernandoStark18 <fernandostark18@gmail.com>
2022-07-20 19:34:52 -0600

    Objetos

---
+++ js/12_objetos.js
@@ -0,0 +1,15 @@
+// Objetos
+
+const productName = "Monitor 20\"";
+const price = 300;
+const available = true;
+
+const product = {
+    productName: "Monitor 20\"",
+    price: 300,
+    available: true
+}
+
+console.log(product);
+
+console.log(`Nombre del producto: ${product.productName}, Precio: ${product.price}, Disponibilidad: ${product.available}`);
