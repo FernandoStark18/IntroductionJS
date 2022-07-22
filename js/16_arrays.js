f127d9d56b904529c9743c340c0d99bec5094427
FernandoStark18 <fernandostark18@gmail.com>
2022-07-21 20:00:43 -0600

    Métodos para arrays

--- js/16_arrays.js
+++ js/16_arrays.js
@@ -1,19 +1,40 @@
 // Arrays are objects
 
 const numbers = [10, 20, 30, 40, 50];
+console.table(numbers);
+
 const months = new Array('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre');
-const array = ['Hi', 10, true, 'Sí', null, {nombre: 'Fernando', trabajo: 'Programador'}, [1, 2, 3]];
+console.table(months);
 
-console.log(`Var: ${numbers} Type: ${typeof numbers}`);
-console.log(`Var: ${months} Type: ${typeof months}`);
-console.log(array);
+// const array = ['Hi', 10, true, 'Sí', null, {nombre: 'Fernando', trabajo: 'Programador'}, [1, 2, 3]];
 
-// Acceder a valores de un arreglo
-console.log(numbers[4]);
+// console.log(`Var: ${numbers} Type: ${typeof numbers}`);
+// console.log(`Var: ${months} Type: ${typeof months}`);
+// console.log(array);
 
-// Conocer la extensión de un arreglo
-console.log(numbers.length);
+// // Acceder a valores de un arreglo
+// console.log(numbers[4]);
 
-numbers.forEach( function(number){
-    console.log(number);
-})
+// // Conocer la extensión de un arreglo
+// console.log(numbers.length);
+
+// numbers.forEach( function(number){
+//     console.log(number);
+// })
+
+numbers[5] = 60;
+numbers[6] = 70;
+console.table(numbers);
+
+numbers.push(80);
+numbers.push(80,100,110);
+numbers.unshift(-10, -20, -30);
+console.table(numbers);
+
+months.pop();
+months.shift();
+months.splice(2, 1);
+console.table(months);
+
+const newArray = [ ...months, 'Jupiter'];
+console.table(newArray);
