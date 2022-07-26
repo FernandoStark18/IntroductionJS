a64561cafbe535602ae891b08ce8399066e8a0e9
FernandoStark18 <fernandostark18@gmail.com>
2022-07-25 20:35:03 -0600

    Funciones en JavasScript

---
+++ js/18_funciones.js
@@ -0,0 +1,19 @@
+// Declaracion de una Función
+function sumar() {
+    console.log(11 * 11);
+}
+
+sumar();
+
+// Expresión de la funcion 
+const sumar2 = function (){
+    console.log(3 + 3);
+}
+
+sumar2();
+
+// IIFE
+(function() { 
+    console.log('This is a function');
+})();
+
