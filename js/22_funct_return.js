2c7933cf8c67df0f4baf2b7dc32036c377e62d92
FernandoStark18 <fernandostark18@gmail.com>
2022-07-25 21:23:21 -0600

    Funciones que retornan valores

---
+++ js/22_funct_return.js
@@ -0,0 +1,25 @@
+function sumar(n1, n2) {
+    return n1 + n2;
+}
+
+const resultado = sumar(2, 3);
+console.log(resultado);
+
+let total = 0;
+
+function agregarCarrito(precio) {
+    return total += precio;
+}
+
+function calcularImpuesto(total) {
+    return 1.16 * total;
+}
+
+total = agregarCarrito(200);
+total = agregarCarrito(400);
+total = agregarCarrito(600);
+console.log(`Subtotal: ${total}`);
+
+const totalAPagar = calcularImpuesto(total);
+console.log(`Total: ${totalAPagar}`);
+
