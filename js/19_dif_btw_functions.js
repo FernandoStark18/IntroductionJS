4093b1d160193b959c323fe072ab50c55834def7
FernandoStark18 <fernandostark18@gmail.com>
2022-07-25 20:43:51 -0600

    Hoisting

---
+++ js/19_dif_btw_functions.js
@@ -0,0 +1,11 @@
+// Declaracion de una Función
+sumar();
+function sumar() {
+    console.log(11 * 11);
+}
+
+// Expresión de la funcion 
+sumar2();
+const sumar2 = function (){
+    console.log(3 + 3);
+}
