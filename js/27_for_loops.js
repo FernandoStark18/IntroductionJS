f4075af164afb6696016d26c7f41dc5a960cbca6
FernandoStark18 <fernandostark18@gmail.com>
2022-07-27 20:49:28 -0600

    While and Do While

--- js/27_for_loops.js
+++ js/27_for_loops.js
@@ -1,7 +1,8 @@
 // For Loop
 
-for ( let i = 0; i <= 100; i++ ) {
-    if ( i % 2 == 0 ) {
+console.log('FOR LOOP \n\n');
+for (let i = 0; i <= 10; i++) {
+    if (i % 2 == 0) {
         console.log(`El número ${i} es PAR`);
     } else {
         console.log(`El número ${i} es IMPAR`);
@@ -19,6 +20,45 @@ const car = [
     { name: 'Laptop', price: 800 }
 ];
 
-for ( let i = 0; i <= car.length; i++) {
+for (let i = 0; i < car.length; i++) {
     console.log(car[i].name);
 }
+
+// While Loop
+
+console.log('WHILE LOOP \n\n');
+let i = 0;
+
+while (i <= 10) {
+    if (i % 2 == 0) {
+        console.log(`El número ${i} es PAR`);
+    } else {
+        console.log(`El número ${i} es IMPAR`);
+    }
+    i++;
+}
+
+i = 0;
+while (i < car.length) {
+    console.log(`Artículo ${i + 1}: ${car[i].name}`);
+    i++;
+}
+
+// Do while Loop
+
+console.log('DO WHILE LOOP\n\n');
+i = 0;
+do {
+    if (i % 2 == 0) {
+        console.log(`El número ${i} es PAR`);
+    } else {
+        console.log(`El número ${i} es IMPAR`);
+    }
+    i++;
+} while (i <= 10);
+
+i = 0;
+do {
+    console.log(`Artículo ${i + 1}: ${car[i].name}`);
+    i++;
+} while (i < car.length);
