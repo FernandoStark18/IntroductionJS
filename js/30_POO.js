f57504f5ba904838731a5d99cdb3a66575034fe7
FernandoStark18 <fernandostark18@gmail.com>
2022-07-28 20:59:39 -0600

    *Prototypes*

--- js/30_POO.js
+++ js/30_POO.js
@@ -12,7 +12,33 @@ function Producto(nombre, precio) {
     this.precio = precio;
 }
 
+// Prototype nos permite crear funciones que solo se utilizan en un objeto en específico
+
+Producto.prototype.formatearProducto = function() {
+    return `${this.nombre} tiene un precio de: ${this.precio}`;
+}
+Producto.prototype.obtenerPrecio = function() {
+    return `El precio es de: ${this.precio}`;
+}
+
+function Cliente(nombre, apellido) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+}
+
+Cliente.prototype.formatearCliente = function() {
+    return `$El Cliente {this.nombre} ${this.apellido}`;
+}
+
 const producto2 = new Producto('Monitor Curvo de 49"', 800);
 const producto3 = new Producto('Laptop', 300);
-console.log(producto2);
-console.log(producto3);
+const cliente = new Cliente('Fernando', 'Espericueta');
+
+function formatearProducto(producto) {
+    return `${producto.nombre} tiene un precio de: ${producto.precio}`
+}
+
+console.log(producto2.formatearProducto());
+console.log(producto3.formatearProducto());
+console.log(cliente.formatearCliente());
+console.log(producto2.obtenerPrecio());
